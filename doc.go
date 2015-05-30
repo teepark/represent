c399e4676e65cf13948ab409bff4eea098dd718a
Travis J Parker <travis.parker@gmail.com>
2015-05-29 19:59:56 -0700

    package-level docs

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package represent provides a plug-in mechanism for serialization formats and
+// matches against the Accept and Content-Type headers of http.Requests to make
+// it easy to support a variety of representations of HTTP resources.
+package represent
