dae509dac05f5c4cdb2cbeb662d2a50c5f35dc79
Travis J Parker <travis.parker@gmail.com>
2015-06-04 09:42:29 -0700

    more specific names for http-related functions

--- http.go
+++ http.go
@@ -10,25 +10,25 @@ import (
 // ErrNoMatch is for when no suitable protocol is found
 var ErrNoMatch = errors.New("no protocol matched the request")
 
-// Decode uses an appropriate protocol to decode an http request body into a
-// container. It returns a suggested HTTP response code and an error (if the
-// error is nil the response code will be StatusOK, 200).
-func Decode(container interface{}, r *http.Request) (int, error) {
-	return globalReg.Decode(container, r)
+// DecodeRequest uses an appropriate protocol to decode an http request body
+// into a container. It returns a suggested HTTP response code and an error (if
+// the error is nil the response code will be StatusOK, 200).
+func DecodeRequest(container interface{}, r *http.Request) (int, error) {
+	return globalReg.DecodeRequest(container, r)
 }
 
-// Encode will serialize data with a format acceptable by the HTTP client
-// (using the Accept header of the request). If there is an error it will NOT
-// send the response but will instead return a suggested HTTP response code and
-// the error. If it returns (200, nil) then the data has already been written
-// to the response.
-func Encode(data interface{}, r *http.Request, w http.ResponseWriter) (int, error) {
-	return globalReg.Encode(data, r, w)
+// EncodeResponse will serialize data with a format acceptable by the HTTP
+// client (using the Accept header of the request). If there is an error it
+// will NOT send the response but will instead return a suggested HTTP response
+// code and the error. If it returns (200, nil) then the data has already been
+// written to the response.
+func EncodeResponse(data interface{}, r *http.Request, w http.ResponseWriter) (int, error) {
+	return globalReg.EncodeResponse(data, r, w)
 }
 
-// Decode as a Registry method performs the same job as the global function,
-// but using the protocols registered with a specific registry.
-func (reg *Registry) Decode(container interface{}, r *http.Request) (int, error) {
+// DecodeRequest as a Registry method performs the same job as the global
+// function, but using the protocols registered with a specific registry.
+func (reg *Registry) DecodeRequest(container interface{}, r *http.Request) (int, error) {
 	protocol, err := reg.Match(r.Header.Get("Content-Type"))
 	if err != nil {
 		return http.StatusBadRequest, err
@@ -46,9 +46,9 @@ func (reg *Registry) Decode(container interface{}, r *http.Request) (int, error)
 	return http.StatusOK, nil
 }
 
-// Encode performs the same job as the global function, but matches against the
-// set of Protocols registered with the specific registry.
-func (reg *Registry) Encode(data interface{}, r *http.Request, w http.ResponseWriter) (int, error) {
+// EncodeResponse performs the same job as the global function, but matches
+// against the set of Protocols registered with the specific registry.
+func (reg *Registry) EncodeResponse(data interface{}, r *http.Request, w http.ResponseWriter) (int, error) {
 	protocol, err := reg.Match(r.Header.Get("Accept"))
 	if err != nil {
 		return http.StatusBadRequest, err
